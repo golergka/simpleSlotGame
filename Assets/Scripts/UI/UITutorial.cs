814b6eaf2a44c546c571c3965d46037f70fd94ce
Max Yankov <golergka@gmail.com>
2015-01-19 14:33:46 +0200

    A simple tutorial panel

---
+++ Assets/Scripts/UI/UITutorial.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UITutorial : MonoBehaviour
+{
+	#region Configuration
+
+	public Game Game;
+
+	#endregion
+
+	#region Engine methods
+
+	void Start()
+	{
+		System.Action<Spin> onSpin = delegate(Spin _Spin)
+		{
+			Game.OnSpin -= onSpin;
+			Destroy(gameObject);
+		};
+		Game.OnSpin += onSpin;
+	}
+
+	#endregion
+}

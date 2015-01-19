c7b04e57193ff189f6863f4327ac5e556dfd334d
Max Yankov <golergka@gmail.com>
2015-01-19 14:29:29 +0200

    Win panel with animations

---
+++ Assets/Scripts/UI/UIGreeting.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class UIGreeting : MonoBehaviour
+{
+	#region Helper properties
+
+	Animator m_Animator;
+	Animator Animator
+	{
+		get
+		{
+			if (m_Animator == null)
+			{
+				m_Animator = GetComponent<Animator>();
+			}
+			return m_Animator;
+		}
+	}
+
+	#endregion
+
+	#region Configuration
+
+	public Text Text;
+	public Game Game;
+
+	#endregion
+
+	#region Engine methods
+
+	void Start()
+	{
+		Game.OnSpin += delegate(Spin _Spin)
+		{
+			Animator.SetInteger("TotalWin", _Spin.TotalWin);
+			Text.text = _Spin.TotalWin.ToString();
+		};
+	}
+
+	#endregion
+}
